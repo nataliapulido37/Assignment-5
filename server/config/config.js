0bb22f57d35318cc6218f38c45f41c7380e9580b
nataliapulido37 <nataliapulido37@ufl.edu>
2017-09-23 13:13:00 -0400

    ignore config file

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  db: {
-    uri: 'mongodb://nataliapulido37:Nxp8917744@ds129144.mlab.com:29144/cen3031', //place the URI of your mongo database here.
-  }, 
-  googleMaps: {
-    key: ''
-  },
-  port: process.env.PORT || 8080
-};
